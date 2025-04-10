3e56b01fe454daa92fae0251bf543d13478e5f50
JavierLU90 <jlomelinu@gmail.com>
2025-04-10 16:56:29 -0600

    Finish structs

--- structs/coord.c
+++ structs/coord.c
@@ -1,10 +1,16 @@
 #include "coord.h"
 
 struct Coordinate new_coord(int x, int y, int z) {
-    struct Coordinate c = {
-        .x = x,
-        .y = y,
-        .z = z
-    };
-    return c;
+  coordinate_t coord = {.x = x, .y = y, .z = z};
+
+  return coord;
+}
+
+struct Coordinate scale_coordinate(struct Coordinate coord, int factor) {
+  coordinate_t scaled = coord;
+  scaled.x *= factor;
+  scaled.y *= factor;
+  scaled.z *= factor;
+
+  return scaled;
 }
